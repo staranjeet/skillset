2b948f9243cef5103731013720701007fe6ddedc
Taranjeet <reachtotj@gmail.com>
2015-03-09 01:03:01 +0530

    mods for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn skillset.wsgi --log-file -
--- skillset/settings.py
+++ skillset/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 ADMINS = (
@@ -81,7 +82,7 @@ DATABASES = {
         'PASSWORD':'gameover',
     }
 }
-
+DATABASES['default'] =  dj_database_url.config()
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
